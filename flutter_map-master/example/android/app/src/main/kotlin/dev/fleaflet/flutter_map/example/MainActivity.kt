5f141309abc22352dd16134dcd34a51220e27a20
Darshan <149821544+Darshan-hegde04@users.noreply.github.com>
2023-12-13 22:04:10 +0530

    Updated

--- flutter_map-master/example/android/app/src/main/kotlin/dev/fleaflet/flutter_map/example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package dev.fleaflet.flutter_map.example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flutter_map-master/test/test_utils/test_tile_image.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:convert';
-
-import 'package:flutter/painting.dart';
-
-// Base 64 encoded 256x256 white tile.
-const _whiteTile =
-    'iVBORw0KGgoAAAANSUhEUgAAAQAAAAEAAQMAAABmvDolAAAAAXNSR0IB2cksfwAAAAlwSFlzAAALEwAACxMBAJqcGAAAAANQTFRF////p8QbyAAAAB9JREFUeJztwQENAAAAwqD3T20ON6AAAAAAAAAAAL4NIQAAAfFnIe4AAAAASUVORK5CYII=';
-final testWhiteTileBytes = base64Decode(_whiteTile);
-final testWhiteTileImage = MemoryImage(testWhiteTileBytes);
